77a8cfc642098acc40270d940e1d5f61790106e6
Максим Володько <maksimvolodko41@gmail.com>
2025-05-16 13:35:49 +0300

    Добавлены структуры для хранения данных

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Володько Максим Сергеевич (в лабе нет изменённой программы)";
+    std::cout << "Volod'ko Maksim Sergeevich 15.05.2025 17.05.2025 Lev Nikolaevich Tolstoy Voyna i mir";
     return 0;
 }
